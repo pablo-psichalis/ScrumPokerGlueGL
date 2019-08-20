3bbb251f0079e1a5b24cd8f02eb0fefb5122be5c
pablo-psichalis <pablo.psrz@gmail.com>
2019-08-20 20:42:47 +0200

    Added Router & empty views

---
+++ scrum-poker/src/components/Router/index.js
@@ -0,0 +1,27 @@
+
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import App from '../../layouts/App'
+import Home from '../../views/Home'
+import Voting from '../../views/VotingView'
+import Loading from '../../views/LoadingView'
+import Results from '../../views/ResultsView'
+
+import NotFound from '../NotFound'
+
+const Router = ({ history }) => (
+    <BrowserRouter>
+        <App>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/voting" component={Voting} />
+                <Route path="/loading" component={Loading} />
+                <Route path="/results" component={Results} />
+                <Route component={NotFound} />
+            </Switch>
+        </App>
+    </BrowserRouter>
+)
+
+export default Router
--- scrum-poker/src/layouts/App.js
+++ scrum-poker/src/layouts/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import NameInput from './components/NameInput';
 
 function App() {
   return (
     <div className="App">
-      <NameInput></NameInput>
+      Hello world!
     </div>
   );
 }
